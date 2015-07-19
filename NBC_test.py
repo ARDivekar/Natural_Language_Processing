d637d9b5d00ad54c9e591c4a5aa5c5ebb802f2a5
ARDivekar <abhishek.r.divekar@gmail.com>
2015-07-19 23:49:40 +0530

    Dump of code files and other files
    Code files:
    NBC_train.py - a file to train a Naive Bayes classifier for a machine
    learning task
    NBC_test.py - code to test the above NBC
    
    Data files:
    all_sentences.pickle - a Python list of a lot of sentences, taken from a
    large repository of Infosys and Yes Bank articles (yes, the one in the
    Extracted_Articles repo)
    all_sentences_shuffled.pickle - the above, but shuffled. We actually use
    this one in NBC_train.py
    training_set.pickle - the set we have trained so far. NBC_test.py uses
    this as its training set to classify sentences.

---
+++ NBC_test.py
@@ -0,0 +1,181 @@
+import pickle
+import nltk
+from collections import defaultdict
+from random import randrange
+from math import log
+# from NBC_train import Training_set
+
+
+def make_NGRAMS(n, text): 
+	# n is the MAX gram depth
+	# assume 'text' is a list of words
+	Grams={}
+	for i in range(1,n+1): # i is the gram-depth, i.e. unigrams, bigrams, trigrams, quadrigrams etc.
+		
+		Grams[i]=defaultdict(int)  # dict of tuples, where each tuple is a key for its correpsonding occurence count.
+		# eg: Grams[4] = {('I', 'want', 'some', 'chinese'): 14, ('hello', ',', 'can', 'you'): 8)}
+		for j in range(i, len(text)):
+			last_i_words=[]
+			for k in range(0,i):
+				last_i_words.append(text[j-i+k])
+			last_i_words=tuple(last_i_words)  # to be used as a key for dictionary Grams[i]
+			Grams[i][last_i_words]=Grams[i][last_i_words]+1
+	return Grams
+
+
+
+class Training_set:
+	def __init__(self, i, training):
+		self.i=i
+		self.training=training
+
+with open("F:\Workspaces\Python\Stock Price Application\Articles/all_sentences_shuffled.pickle", 'rb') as handle:
+	all_sentences=pickle.load(handle)
+
+with open("F:\Workspaces\Python\Stock Price Application\Articles/training_set.pickle", 'rb') as handle:
+	training_set_obj=pickle.load(handle)
+
+i=training_set_obj.i
+print "\n\tNumber of marked sentences: %s"%i
+training=training_set_obj.training
+# for i in training:
+# 	print "%s : %s"%(i,len(training[i]))
+
+
+N=int(raw_input("\nEnter N:\n>"))
+
+def NB_preprocess():
+	d=5
+
+
+
+vocab=[]
+classes_len={}  #the total number of words in each class
+classes_len[0]=0
+classes_len[1]=0
+classes_len[2]=0
+classes_len[3]=0
+classes_len[4]=0
+classes_len[5]=0
+
+classes={}   #a lot of data about each class
+# classes[0]=defaultdict(int)
+# classes[1]=defaultdict(int)
+# classes[2]=defaultdict(int)
+# classes[3]=defaultdict(int)
+# classes[4]=defaultdict(int)
+# classes[5]=defaultdict(int)
+
+mega_docs=defaultdict(lambda:[])
+classes_grams={}
+
+for x in training: #training is a dictionary of all the sentences in that class
+	for y in training[x]:
+		words=nltk.word_tokenize(y)
+		vocab+=words
+		classes_len[x]+=len(words)
+		mega_docs[x]+=(words)
+
+		# for z in words:
+		# 	classes[x][z]+=1  #the unigram counts of the classes
+	classes_grams[x]=make_NGRAMS(N, mega_docs[x])
+
+
+#classes_grams is a dictionary of dictionaries of dictionaries.
+#Top level: for each class, we have a dictionary of N-Grams for the sentences in that class.
+#2nd level: inside each of these dictionaries, we index by the 'N' of N-gram, to get a dictionary of counts of that N-gram model.
+#3rd level: at this level, we index by unigrams, bigrams, trigrams etc, to get their counts.
+
+
+vocab=set(vocab)
+
+print "vocab size:%s"%len(vocab)
+# for x in classes_len:
+# 	print "Class %s : Number of words = %s, Number of unique words = %s"%(x,classes_len[x], len(classes[x]))
+
+
+
+
+def len_unigrams_to_ngrams(number_of_unigrams, N):
+	return number_of_unigrams-N+1
+
+
+def NBclassify_Ngrams(training, classes_grams, classes_len, sentence, N):
+	vocabs=defaultdict(lambda:[])  #vocabulary for unigrams, bigrams etc.
+	for _class_ in classes_grams: 
+		for n in classes_grams[_class_]:  #n for the gram value
+			vocabs[n]+=list(set(classes_grams[_class_][n]))
+			vocabs[n]=list(set(vocabs[n]))
+	vocab_sizes={}	
+	for n in vocabs:
+		vocab_sizes[n]=len(vocabs[n])
+		print vocab_sizes[n]
+
+
+	priors={}
+	total=0  #total number of documents (i.e. sentences)
+	for x in training:
+		total+=len(training[x])
+		priors[x]=len(training[x])
+
+	probabilities={}
+	for x in training:
+		probabilities[x]=log(float(priors[x])/total)
+
+	words=nltk.word_tokenize(sentence)
+	gram_depth=len(classes_grams[0])
+	words_gram=make_NGRAMS(gram_depth, words)
+	# for i in words_gram:
+	# 	print "WORDSGRAM", i
+	# 	print words_gram[i]
+	#N=2 #we only consider bigrams for now
+	for x in range(0,6):
+		for i in words_gram[N]: 
+			count_in_class=classes_grams[x][N][i]
+			probabilities[x]+=log(count_in_class + 1) - log(len_unigrams_to_ngrams(classes_len[x], N) + vocab_sizes[N] + 1)
+	for x in probabilities:
+		print x,":",probabilities[x]
+	selected_class=0
+	for x in range(1,6):
+		if probabilities[x]>probabilities[selected_class]:
+			selected_class=x
+	list_of_classes={0:"Irrelevant", 1:"Very bad", 2:"Bad", 3:"Neutral", 4:"Good", 5:"Very Good"}
+	print "selected_class: %s"%selected_class, list_of_classes[selected_class]
+
+	
+
+
+'''
+def NBclassify(vocab, training, classes_len, classes, sentence):
+	vocab_size=len(vocab)
+	priors={}
+	total=0  #total number of documents (i.e. sentences)
+	for x in training:
+		total+=len(training[x])
+		priors[x]=len(training[x])
+
+	probabilities={}
+	for x in training:
+		probabilities[x]=log(float(priors[x])/total)
+
+	words=nltk.word_tokenize(sentence)
+	for word in words:
+		for x in range(0, 6) : #classes 0 upto 5
+			count_in_class=classes[x][word]  #since all classes[x] are defaultdicts, if word does not occur, then it defaults to zero
+			probabilities[x]+=log(count_in_class +1) - log(classes_len[x]+ vocab_size+1)  #unknowns are considered
+			# probabilities[x]-=log(classes_len[x]/100)
+	for x in probabilities:
+		print x,":",probabilities[x]
+	selected_class=0
+	for x in range(1,6):
+		if probabilities[x]>probabilities[selected_class]:
+			selected_class=x
+	print "selected_class: %s"%selected_class
+
+'''
+
+
+sentence=all_sentences[randrange(i+1,len(all_sentences))]
+print sentence
+# NBclassify(vocab, training, classes_len, classes, sentence)
+NBclassify_Ngrams(training, classes_grams, classes_len, sentence,N)
---
+++ NBC_train.py
@@ -0,0 +1,74 @@
+#Naive Bayes classifier.
+
+#This classifier is trained on sentences which are tagged as very good, good, neutral, bad and very bad.
+#Along with these class tags, also tagged are the words which are used to make this decision. 
+
+import pickle
+import nltk
+from article_manip import Article
+from article_manip import make_sentences
+from random import shuffle
+from collections import defaultdict
+'''
+all_sentences=[]
+all_sentences+=make_sentences('Infosys')
+all_sentences+=make_sentences('Yes Bank')
+with open("F:\Workspaces\Python\Stock Price Application\Articles/all_sentences.pickle", 'wb') as handle:
+	pickle.dump(all_sentences,handle)
+'''
+with open("F:\Workspaces\Python\Stock Price Application\Articles/all_sentences_shuffled.pickle", 'rb') as handle:
+	all_sentences=pickle.load(handle)
+
+class Training_set:
+	def __init__(self, i, training):
+		self.i=i
+		self.training=training
+
+
+with open("F:\Workspaces\Python\Stock Price Application\Articles/training_set.pickle", 'rb') as handle:
+	training_set_obj=pickle.load(handle)
+
+i=training_set_obj.i
+print "\n\tNumber of previously marked sentences: %s"%i
+training=training_set_obj.training
+'''
+training={}
+training[0]=[]
+training[1]=[]
+training[2]=[]
+training[3]=[]
+training[4]=[]
+training[5]=[]
+
+i=0
+'''
+
+while i<len(all_sentences):
+	# print "\n\n\nRate the following sentence as (0=irrelevant, 1=very bad, 2=bad, 3=neutral, 4=good, 5=very good:\n"
+	print "\n\n\nRate the sentence as: \n0=Cannot be rated due to insufficient data \n1=very bad \n2=bad \n3=neutral \n4=good \n5=very good:\n"
+	print all_sentences[i]
+	rating=raw_input('>')
+	prev = i
+	while(prev==i):
+		try:
+			rating  = int(rating)
+			if rating != 0 and rating != 1 and rating != 2 and rating != 3 and rating != 4 and rating != 5:
+				rating=raw_input("\nSorry, that is not a valid input. Try again: ")
+			else:
+				# print "rating:%s"%rating
+				training[rating].append(all_sentences[i])
+				i+=1
+		except Exception: 
+			rating=raw_input("\nSorry, that is not a valid input. Try again: ")
+	
+	if(i%10==0):
+		training_set_obj=Training_set(i,training)
+		with open("F:\Workspaces\Python\Stock Price Application\Articles/training_set.pickle", 'wb') as handle:
+			pickle.dump(training_set_obj,handle)
+		yorn=raw_input("\n\n\n\n\t\t<---EXIT?---> Y or N?\n\t\t>")
+		if yorn.lower()=='y':
+			# print training_set_obj.training
+			print "\nNumber of marked sentences: %s"%training_set_obj.i
+			exit()
+
+
